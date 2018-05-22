985a282e2f83301b9384bb67c6f3dda9adc48e87
Anderson Araújo <anderson.9aa@gmail.com>
2018-05-22 13:10:13 -0300

    First commit

---
+++ js/custom.js
@@ -0,0 +1,12 @@
+﻿(function () {
+    'use strict';
+    /*-----------------------------------
+    One Page Nav
+    -----------------------------------*/
+    if ($('#one-page-nav').length > 0) {
+        $('#one-page-nav').onePageNav({
+            currentClass: 'active',
+            filter: ':not(#search-toggle):not(.pages)'
+        });
+    }
+})();
